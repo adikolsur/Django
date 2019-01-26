d4919e35cb36112a78d8773bd46e2fb7bb52db4e
Aditya Kolsur <adikolsur@live.in>
2019-01-26 12:08:19 +0530

    .

--- firstProject/firstApp/views.py
+++ firstProject/firstApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from firstApp.models import Topic,WebPage,AccessRecord
 # Create your views here.
 
 def index(req):
-    my_dict={'insert_here':"Whaatsup Pt2?"}
+    page_list=AccessRecord.objects.order_by('date')
+    my_dict={'access_record':page_list}
     return render(req,'firstApp/index.html',context=my_dict)
 
 def help(req):
