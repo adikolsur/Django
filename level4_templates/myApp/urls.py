45e10f2772952c0125cd4a0468982965d5046ef3
Aditya Kolsur <adikolsur@live.in>
2019-01-27 18:03:09 +0530

    .

--- level4_templates/myApp/urls.py
+++ level4_templates/myApp/urls.py
@@ -1,6 +1,7 @@
 from myApp import views
 from django.urls import path
 
+app_name = 'myApp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('relative/', views.relative, name='relative'),
