32b0d4c6916a8ca822a3f9da5eef56ce6a842cfa
Aditya Kolsur <adikolsur@live.in>
2019-01-27 23:00:20 +0530

    .

--- level4_templates/myApp/templatetags/my_custom.py
+++ level4_templates/myApp/templatetags/my_custom.py
@@ -2,7 +2,8 @@ from django import template
 
 register=template.Library()
 
+@register.filter(name='cut_string')
 def cut_string(value,arg):
     return value.replace(arg,'')
 
-register.filter('cut_string',cut_string)
+#register.filter('cut_string',cut_string)
